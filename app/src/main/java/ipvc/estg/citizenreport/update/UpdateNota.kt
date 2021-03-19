b9752e3ec6eef0831ac96be535ae203405fc5fab
monicalima18666 <61920974+monicalima18666@users.noreply.github.com>
2021-03-19 20:39:56 +0000

    Editar nota , delete nota - FEITO

--- app/src/main/java/ipvc/estg/citizenreport/update/UpdateNota.kt
+++ app/src/main/java/ipvc/estg/citizenreport/update/UpdateNota.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import ipvc.estg.citizenreport.R
 import ipvc.estg.citizenreport.adapters.DESCRICAO
@@ -44,13 +45,16 @@ class UpdateNota : AppCompatActivity() {
     fun editarNota(view: View) {
         update_titulo = findViewById(R.id.update_titulo)
         update_descricao = findViewById(R.id.update_descricao)
-        var message3 = intent.getIntExtra(Build.ID, 0)
+        var item = intent.getIntExtra(Build.ID, 0)
         val replyIntent = Intent()
         if (TextUtils.isEmpty(update_titulo.text) || TextUtils.isEmpty(update_descricao.text))  {
             setResult(Activity.RESULT_CANCELED, replyIntent)
+            Toast.makeText(applicationContext,"Não atualizada , campos vazios", Toast.LENGTH_LONG).show()
         } else {
-            val nota = Nota(id = message3, titulo = update_titulo.text.toString(), descricao = update_descricao.text.toString())
+            val nota = Nota(id = item, titulo = update_titulo.text.toString(), descricao = update_descricao.text.toString())
             notaViewModel.editNota(nota)
+
+            Toast.makeText(applicationContext,"Atualizada com sucesso",Toast.LENGTH_LONG).show()
 
         }
         finish()
