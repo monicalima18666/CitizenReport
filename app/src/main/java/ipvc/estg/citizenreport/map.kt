5c96da65299ea5f2954a81ffb7721b2821c05734
monicalima18666 <61920974+monicalima18666@users.noreply.github.com>
2021-05-06 19:34:51 +0100

    commit

--- app/src/main/java/ipvc/estg/citizenreport/map.kt
+++ app/src/main/java/ipvc/estg/citizenreport/map.kt
@@ -22,7 +22,7 @@ class map : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-// mapa4
+// mapa5
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewmap)
         val request = ServiceBuilder.buildService(EndPoints::class.java)
         val call = request.getUsers()
